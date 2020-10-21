154be929a07605548fb61a0ae26f9ef00bb7a5ec
Ahmed Abdulkader <ahmed-public@hotmail.com>
2020-10-21 04:21:52 +0300

    update wsgi file

--- twitterclone/wsgi.py
+++ twitterclone/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tweetme.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twitterclone.settings')
 
 application = get_wsgi_application()
